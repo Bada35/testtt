b49391a391634c279ad224f94d3366019776d62b
서지수 <diduedidue@naver.com>
2024-07-23 17:41:59 +0900

    Feat: 초기 설정

--- flip-test/src/renderer/src/App.jsx
+++ flip-test/src/renderer/src/App.jsx
@@ -1,35 +1,29 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import React, { useState } from 'react';
+import MOASS from './assets/이음이.svg';
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+  const [position, setPosition] = useState({ top: 50, left: 50 });
+
+  const handleClick = () => {
+    const randomTop = Math.random() * 90;
+    const randomLeft = Math.random() * 90;
+    setPosition({ top: randomTop, left: randomLeft });
+  };
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
+    <div className="relative w-full h-screen bg-gray-100 flex items-center justify-center">
+      <div 
+        className="absolute transition-all duration-500 ease-in-out transform hover:scale-110"
+        style={{ top: `${position.top}%`, left: `${position.left}%` }}
+        onClick={handleClick}
+      >
+        <img src={MOASS} alt="MOASS character" className="w-16 h-16 cursor-pointer" />
       </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
+      <div className="absolute bottom-4 left-4 text-gray-500">
+        <span>Click the character to play!</span>
       </div>
-      <Versions></Versions>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
-
+export default App;
